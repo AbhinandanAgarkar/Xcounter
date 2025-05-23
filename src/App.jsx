ad59e2bdb51a61830fde2624483c01aa6b6dcdc0
Abhinandan Agarkar <agarkarabhinandan333@gmail.com>
2025-05-23 23:08:22 +0530

    On click functions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-
+import { useState } from "react"
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = ()=>{
+    setCount(count+1);
+  }
+
+    const handledecrement = ()=>{
+    setCount(count-1);
+  }
+
+  
 
   return (
     <>
    <h1>Counter App</h1>
-   <p>Count: </p>
-   <button>Increment</button>
-   <button>Decrement</button>
+   <p>Count: {count} </p>
+   <button onClick={handleIncrement}>Increment</button>
+   <button onClick={handledecrement}>Decrement</button>
    </>
   )
 }
